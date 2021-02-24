eba63eefa78d51b7c246c708a136ce3067c45041
Iago Chiapetta <iagochiapetta@gmail.com>
2021-02-24 13:01:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+function enviar1() {
+    alert("Formulário Enviado!!!! Redirecionando...");
+    window.location.href = "https://speedfiber.ddns.net/";
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+}
+function enviar2() {
+    alert("Formulário Enviado!!!! Redirecionando...");
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+}
+function enviar3() {
+    alert("Formulário Enviado!!!! Redirecionando...");
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+}
+function enviar4() {
+    alert("Formulário Enviado!!!! Redirecionando...");
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+}
+function enviar5() {
+    alert("Formulário Enviado!!!! Redirecionando...");
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );
+}
